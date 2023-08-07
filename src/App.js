0ee4d53f75979e56fec8fbe0ff096a2c5f3448d4
Destwood <DestwoodGames@Gmail.com>
2023-08-07 10:23:04 +0300

    deploy changes and modal window fixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
+import Modal from "react-modal";
+import { useDispatch, useSelector } from "react-redux";
+import { modalState } from "./components/store/actions";
 import "./App.css";
+import style from "./components/modal.module.scss";
 
 import Hero from "./components/Hero/Hero";
 import Cart from "./components/Cart/Cart";
@@ -34,10 +38,48 @@ import Header from "./components/Header/Header";
 // fetchData();
 
 function App() {
+  const isVisible = useSelector((state) => state.showModal);
+  const data = useSelector((state) => state.cart);
+  const items = [...data.pizza, ...data.drinks];
+  const dispatch = useDispatch();
+  const price = items.reduce((totalPrice, item) => totalPrice + item.price, 0);
+  if (isVisible) {
+    document.querySelector("body").style.overflowY = "hidden";
+  } else {
+    document.querySelector("body").style.overflowY = "auto";
+  }
+  const closeModal = () => {
+    dispatch(modalState(false));
+  };
   return (
     <div className="App">
-      <Header />
-      <Cart />
+      <Modal
+        isOpen={isVisible}
+        className={style.modal}
+        overlayClassName={style.overlay}
+        onRequestClose={closeModal}
+      >
+        <div className={style.modalHeader}>Your order:</div>
+        <div className={style.modalContent}>
+          {items.map((item) => (
+            <div key={item.name} className={style.cartItem}>
+              <img className={style.img} src={item.img} alt={item.name} />
+              <div className="">
+                <p className={style.title}>{item.name}</p>
+                <p className={style.desc}>{item.desc}</p>
+              </div>
+              <p className={style.price}>{item.price}</p>
+            </div>
+          ))}
+        </div>
+        <div className={style.modalFooter}>
+          <button className={style.orderBtn}>Order: {price} $</button>
+        </div>
+      </Modal>
+      {!isVisible && <Header />}
+
+      {/* <Cart /> */}
+
       <Hero />
       <ProductList />
     </div>
